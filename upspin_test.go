84a8c6ee7fd219f19620e1415fb6692b69a9f8b2
Alberto Cortés <alberto@sourced.tech>
2017-04-07 16:40:48 +0200

    test setup with local, in-memory servers

--- upspin_test.go
+++ upspin_test.go
@@ -1,8 +1,8 @@
 package upspinfs
 
 import (
-	"fmt"
 	"testing"
+	"time"
 
 	. "gopkg.in/check.v1"
 	"upspin.io/bind"
@@ -19,104 +19,143 @@ func Test(t *testing.T) { TestingT(t) }
 
 type UpspinSuite struct {
 	//test.FilesystemSuite
-	userName upspin.UserName
-	cfg      upspin.Config
+	cfg upspin.Config
 }
 
 var _ = Suite(&UpspinSuite{})
 
+// fixtures
+var (
+	userName = upspin.UserName("user1@google.com")
+	// from $GOPATH/src/upspin.io/key/testdata/user1/
+	publicKey = upspin.PublicKey(`p256
+104278369061367353805983276707664349405797936579880352274235000127123465616334
+26941412685198548642075210264642864401950753555952207894712845271039438170192
+`)
+	// We will use in-memory, non-persistent key, dir and store servers.
+	// The InProcess transport is just perfect for them; it ignores addresses,
+	// this is, all servers will be at the same endpoint:
+	serverEP = upspin.Endpoint{Transport: upspin.InProcess}
+)
+
 func (s *UpspinSuite) SetUpSuite(c *C) {
-	s.userName = upspin.UserName("user1@google.com")
-	s.cfg = inprocessConfig(s.userName, "")
+	s.setUpClientConfig(c)
+	s.assertConfig(c)
+
+	setUpServers(c, s.cfg)
+	s.assertUser(c)
+	s.assertEmptyHome(c)
 }
 
-func (s *UpspinSuite) SetUpTest(c *C) {
-	//client := client.New(s.cfg)
-	//s.FilesystemSuite.Fs = New(client, s.userName)
+// Initialize the client config in its receiver to the fixture values.
+func (s *UpspinSuite) setUpClientConfig(c *C) {
+	s.cfg = config.New()
+	s.cfg = config.SetPacking(s.cfg, upspin.EEIntegrityPack)
+	s.cfg = config.SetUserName(s.cfg, userName)
+	s.cfg = config.SetKeyEndpoint(s.cfg, serverEP)
+	s.cfg = config.SetStoreEndpoint(s.cfg, serverEP)
+	s.cfg = config.SetDirEndpoint(s.cfg, serverEP)
+
+	f, err := factotum.NewFromDir(testutil.Repo("key", "testdata", "user1"))
+	c.Assert(err, IsNil)
+	s.cfg = config.SetFactotum(s.cfg, f)
 }
 
-func inprocessConfig(userName upspin.UserName, publicKey upspin.PublicKey) upspin.Config {
-	cfg := baseConfig()
-	cfg = config.SetUserName(cfg, userName)
-	key, _ := bind.KeyServer(cfg, cfg.KeyEndpoint())
-	checkTransport(key)
-	dir, _ := bind.DirServer(cfg, cfg.DirEndpoint())
-	checkTransport(dir)
-	if cfg.Factotum().PublicKey() == "" {
-		panic("empty public key")
-	}
-	user := &upspin.User{
-		Name:      userName,
-		Dirs:      []upspin.Endpoint{cfg.DirEndpoint()},
-		Stores:    []upspin.Endpoint{cfg.StoreEndpoint()},
-		PublicKey: cfg.Factotum().PublicKey(),
-	}
-	err := key.Put(user)
-	if err != nil {
-		panic(err)
-	}
-	name := upspin.PathName(userName) + "/"
-	entry := &upspin.DirEntry{
-		Name:       name,
-		SignedName: name,
-		Attr:       upspin.AttrDirectory,
-		Writer:     userName,
-	}
-	_, err = dir.Put(entry)
-	if err != nil {
-		panic(err)
-	}
-	return cfg
-}
-
-func checkTransport(s upspin.Service) {
-	if s == nil {
-		panic(fmt.Sprintf("nil service"))
-	}
-	if t := s.Endpoint().Transport; t != upspin.InProcess {
-		panic(fmt.Sprintf("bad transport %v, want inprocess", t))
-	}
-}
-
-func baseConfig() upspin.Config {
-	inProcess := upspin.Endpoint{
-		Transport: upspin.InProcess,
-		NetAddr:   "", // ignored
-	}
-
-	f, err := factotum.NewFromDir(testutil.Repo("key", "testdata", "user1")) // Always use user1's keys.
-	if err != nil {
-		panic("cannot initialize factotum: " + err.Error())
-	}
-
-	cfg := config.New()
-	cfg = config.SetPacking(cfg, upspin.EEIntegrityPack)
-	cfg = config.SetKeyEndpoint(cfg, inProcess)
-	cfg = config.SetStoreEndpoint(cfg, inProcess)
-	cfg = config.SetDirEndpoint(cfg, inProcess)
-	cfg = config.SetFactotum(cfg, f)
-
+// Runs and registers key, dir, and store servers for a the user
+// described in cfg.  Its home is initialized empty.
+func setUpServers(c *C, cfg upspin.Config) {
 	bind.RegisterKeyServer(upspin.InProcess, keyserver.New())
 	bind.RegisterStoreServer(upspin.InProcess, storeserver.New())
 	bind.RegisterDirServer(upspin.InProcess, dirserver.New(cfg))
 
-	return cfg
+	user := &upspin.User{
+		Name:      cfg.UserName(),
+		Dirs:      []upspin.Endpoint{cfg.DirEndpoint()},
+		Stores:    []upspin.Endpoint{cfg.StoreEndpoint()},
+		PublicKey: cfg.Factotum().PublicKey(),
+	}
+
+	// add user to key server
+	key, err := bind.KeyServer(cfg, cfg.KeyEndpoint())
+	c.Assert(err, IsNil)
+	err = key.Put(user)
+	c.Assert(err, IsNil)
+
+	homePath := upspin.PathName(cfg.UserName() + "/")
+	entry := &upspin.DirEntry{
+		Name:       homePath,
+		SignedName: homePath,
+		Attr:       upspin.AttrDirectory,
+		Writer:     cfg.UserName(),
+	}
+
+	// add a home directory entry for the user at the dir server
+	dir, err := bind.DirServer(cfg, cfg.DirEndpoint())
+	c.Assert(err, IsNil)
+	_, err = dir.Put(entry)
+	c.Assert(err, IsNil)
+}
+
+func (s *UpspinSuite) assertConfig(c *C) {
+	c.Assert(s.cfg.UserName(), Equals, userName)
+	c.Assert(s.cfg.Factotum().PublicKey(), Equals, publicKey)
+	c.Assert(s.cfg.KeyEndpoint(), Equals, serverEP)
+	c.Assert(s.cfg.DirEndpoint(), Equals, serverEP)
+	c.Assert(s.cfg.StoreEndpoint(), Equals, serverEP)
+}
+
+func (s *UpspinSuite) assertUser(c *C) {
+	e := s.cfg.KeyEndpoint()
+	c.Assert(e.Transport, Equals, upspin.InProcess)
+	keyServer, err := bind.KeyServer(s.cfg, e)
+	c.Assert(err, IsNil)
+
+	user, err := keyServer.Lookup(userName)
+	c.Assert(err, IsNil)
+	c.Assert(user.Name, Equals, userName)
+	c.Assert(user.Dirs, DeepEquals, []upspin.Endpoint{serverEP})
+	c.Assert(user.Stores, DeepEquals, []upspin.Endpoint{serverEP})
+	c.Assert(user.PublicKey, Equals, publicKey)
+}
+
+func (s *UpspinSuite) assertEmptyHome(c *C) {
+	e := s.cfg.DirEndpoint()
+	c.Assert(e.Transport, Equals, upspin.InProcess)
+	dirServer, err := bind.DirServer(s.cfg, e)
+	c.Assert(err, IsNil)
+
+	path := upspin.PathName(userName + "/")
+	de, err := dirServer.Lookup(path)
+	c.Assert(err, IsNil)
+	c.Assert(de.IsDir(), Equals, true)
+	c.Assert(de.Writer, Equals, userName)
+	c.Assert(de.Name, Equals, path)
+	c.Assert(len(de.Blocks), Equals, 1)
+	block := de.Blocks[0]
+	c.Assert(block.Offset, Equals, int64(0))
+	c.Assert(block.Size, Equals, int64(0))
+	c.Assert(block.Location.Endpoint, Equals, s.cfg.StoreEndpoint())
+
+	e = s.cfg.StoreEndpoint()
+	c.Assert(e.Transport, Equals, upspin.InProcess)
+	storeServer, err := bind.StoreServer(s.cfg, e)
+	c.Assert(err, IsNil)
+
+	data, refData, locations, err := storeServer.Get(block.Location.Reference)
+	c.Assert(err, IsNil)
+	c.Assert(data, DeepEquals, []byte{})
+	c.Assert(refData.Reference, Equals, block.Location.Reference)
+	c.Assert(refData.Volatile, Equals, false)
+	c.Assert(refData.Duration, Equals, time.Duration(0))
+	c.Assert(locations, IsNil)
+}
+
+func (s *UpspinSuite) SetUpTest(c *C) {
+	s.assertEmptyHome(c)
 }
 
 func (s *UpspinSuite) TearDownTest(c *C) {
 }
 
-func (s *UpspinSuite) TestSetUpSuite(c *C) {
-	keyServer, err := bind.KeyServer(s.cfg, s.cfg.KeyEndpoint())
-	c.Assert(err, IsNil)
-
-	userName := upspin.UserName("user1@google.com")
-	user, err := keyServer.Lookup(userName)
-	c.Assert(err, IsNil)
-
-	c.Assert(user.Name, Equals, userName)
-	fmt.Println(user.Dirs)
-	fmt.Println(user.Stores)
-	fmt.Println(user.PublicKey)
-
+func (s *UpspinSuite) Test(c *C) {
 }
